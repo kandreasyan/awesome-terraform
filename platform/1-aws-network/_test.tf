e46cda60fd53da9efb6dfacabd128503f050f48c
kandreasyan <65715155+kandreasyan@users.noreply.github.com>
2026-03-04 15:47:18 +0400

    Update random_pet resource length and add new resource

--- platform/1-aws-network/_test.tf
+++ platform/1-aws-network/_test.tf
@@ -1,10 +1,14 @@
 resource "random_pet" "test" {
-  length = 2
+  length = 3
 }
 
 ephemeral "random_password" "test" {
   length           = 16
   special          = false
+}
+
+resource "random_pet" "test1" {
+  length = 3
 }
 
 output "pet_name" {
